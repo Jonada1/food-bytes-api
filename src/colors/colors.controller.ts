f26582cc8dafd9cdfbc3bb69996a181118c3f4b0
jonada1 <jonada.ferracaku@gmail.com>
2020-01-29 01:29:43 +0200

    [Color Analyzer] Order colors by population

--- src/colors/colors.controller.ts
+++ src/colors/colors.controller.ts
@@ -3,10 +3,19 @@ import { ImagesService } from '../images/images.service';
 import Vibrant = require('node-vibrant');
 @Controller('colors')
 export class ColorsController {
-    constructor(private readonly imageService: ImagesService) {}
-    @Get(':id')
-    public async getColors(@Param('id') id: string) {
-        const url = await (await this.imageService.getById(id)).url;
-        return await Vibrant.from(url).getPalette();
-    }
+  constructor(private readonly imageService: ImagesService) {}
+  @Get(':id')
+  public async getColors(@Param('id') id: string) {
+    const url = await (await this.imageService.getById(id)).url;
+    const palette = await Vibrant.from(url).getPalette();
+    const paletteOrdered = [
+      palette.DarkMuted,
+      palette.DarkVibrant,
+      palette.LightMuted,
+      palette.LightVibrant,
+      palette.Muted,
+      palette.Vibrant
+    ].sort((palette1, palette2) => palette2.population - palette1.population);
+    return paletteOrdered;
+  }
 }
