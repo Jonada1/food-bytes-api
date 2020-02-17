08929e7daf96fcc87addb26bdbaa0bd52b14f303
jonada1 <jonada.ferracaku@gmail.com>
2020-02-17 12:23:11 +0200

    Delete questionnaire as well

--- src/questionnaire/questionnaire.service.ts
+++ src/questionnaire/questionnaire.service.ts
@@ -7,39 +7,74 @@ import { GetQuestionnaireDto } from './dtos/get-questionnaire.dto';
 
 @Injectable()
 export class QuestionnaireService {
-    constructor(@InjectModel('Questionnaire') private readonly toQuestionnaireModel: Model<Questionnaire>) { }
+  constructor(
+    @InjectModel('Questionnaire')
+    private readonly toQuestionnaireModel: Model<Questionnaire>,
+  ) {}
 
-    async getImageQuestionnaire(imageId: string, userId: string) {
-        return toGetQuestionnaireDto(await this.toQuestionnaireModel.findOne({ imageId, userId }));
-    }
+  async deleteQuestionnaire(imageId: string) {
+    await this.toQuestionnaireModel.deleteOne({ imageId });
+  }
+  async getImageQuestionnaire(imageId: string, userId: string) {
+    return toGetQuestionnaireDto(
+      await this.toQuestionnaireModel.findOne({ imageId, userId }),
+    );
+  }
 
-    async getUserQuestionnaires(userId: string) {
-        return (await this.toQuestionnaireModel.find({userId})).map(toGetQuestionnaireDto);
-    }
+  async getUserQuestionnaires(userId: string) {
+    return (await this.toQuestionnaireModel.find({ userId })).map(
+      toGetQuestionnaireDto,
+    );
+  }
 
-    async getAllQuestionnaires() {
-        return (await this.toQuestionnaireModel.find()).map(toGetQuestionnaireDto);
-    }
+  async getAllQuestionnaires() {
+    return (await this.toQuestionnaireModel.find()).map(toGetQuestionnaireDto);
+  }
 
-    async addQuestionnaireAnswer(postQuestionnaireDto: PostQuestionnaireDto, userId: string) {
-        if(!await this.toQuestionnaireModel.exists({userId, imageId: postQuestionnaireDto.imageId})) {
-            const questionnaire = await (await this.toQuestionnaireModel.create({...postQuestionnaireDto, userId})).save();
-            return toGetQuestionnaireDto(questionnaire);
-        } else {
-            throw new BadRequestException('The answers to this questionnaire already exist')
-        }
+  async addQuestionnaireAnswer(
+    postQuestionnaireDto: PostQuestionnaireDto,
+    userId: string,
+  ) {
+    if (
+      !(await this.toQuestionnaireModel.exists({
+        userId,
+        imageId: postQuestionnaireDto.imageId,
+      }))
+    ) {
+      const questionnaire = await (
+        await this.toQuestionnaireModel.create({
+          ...postQuestionnaireDto,
+          userId,
+        })
+      ).save();
+      return toGetQuestionnaireDto(questionnaire);
+    } else {
+      throw new BadRequestException(
+        'The answers to this questionnaire already exist',
+      );
     }
+  }
 }
 
-function toGetQuestionnaireDto(questionnaire: Questionnaire): GetQuestionnaireDto {
-    const { questionOneAnswer, questionTwoAnswer, questionThreeAnswer, questionFourAnswer, questionFiveAnswer, imageId, userId } = questionnaire;
-    return {
-        imageId,
-        questionFiveAnswer,
-        questionFourAnswer,
-        questionOneAnswer,
-        questionThreeAnswer,
-        questionTwoAnswer,
-        userId
-    };
+function toGetQuestionnaireDto(
+  questionnaire: Questionnaire,
+): GetQuestionnaireDto {
+  const {
+    questionOneAnswer,
+    questionTwoAnswer,
+    questionThreeAnswer,
+    questionFourAnswer,
+    questionFiveAnswer,
+    imageId,
+    userId,
+  } = questionnaire;
+  return {
+    imageId,
+    questionFiveAnswer,
+    questionFourAnswer,
+    questionOneAnswer,
+    questionThreeAnswer,
+    questionTwoAnswer,
+    userId,
+  };
 }
