93059a88b7e82672831c0079113715ec8c1018ab
Jonada Ferracaku <jonada.ferracaku@nortal.com>
2020-04-11 15:04:55 +0300

    Add user in extraction

--- src/extraction/extraction.controller.ts
+++ src/extraction/extraction.controller.ts
@@ -1,7 +1,6 @@
-import { Controller, Get, Req } from '@nestjs/common';
+import { Controller, Get } from '@nestjs/common';
 import { ImagesService } from 'src/images/images.service';
 import { UserService } from 'src/user/user.service';
-import { QuestionnaireService } from 'src/questionnaire/questionnaire.service';
 
 @Controller('extraction')
 export class ExtractionController {
@@ -10,9 +9,9 @@ export class ExtractionController {
   @Get('')
   async getImagesWithQuestionnaires() {
     const users = await this.userService.getAll();
-    const usersTotal: {[key: string]: {questionnaires: any[]; meals: any[]}} = {};
+    const usersTotal: {[key: string]: {questionnaires: any[]; meals: any[], user: any}} = {};
     for(const user of users) {
-      usersTotal[user.id] = {questionnaires: [], meals: []};
+      usersTotal[user.id] = {questionnaires: [], meals: [], user};
       usersTotal[user.id].questionnaires = await this.imagesService.getImagesWithQuestionnaires(user.id);
       usersTotal[user.id].meals = await this.imagesService.getMealsPerDay(user.id);
     };
