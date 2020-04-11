9b7ae741ece3126b3409fd529679ac7c08251da7
Jonada Ferracaku <jonada.ferracaku@nortal.com>
2020-04-11 14:50:24 +0300

    Add extraction module

--- src/extraction/extraction.module.ts
+++ src/extraction/extraction.module.ts
@@ -1,11 +1,14 @@
 import { Module } from '@nestjs/common';
+import { ImagesModule } from 'src/images/images.module';
 import { ImagesService } from 'src/images/images.service';
+import { QuestionnaireController } from 'src/questionnaire/questionnaire.controller';
+import { QuestionnaireModule } from 'src/questionnaire/questionnaire.module';
 import { QuestionnaireService } from 'src/questionnaire/questionnaire.service';
 
 @Module({
-  imports: [],
+  imports: [QuestionnaireModule, ImagesModule],
   providers: [ImagesService, QuestionnaireService],
-  controllers: [],
+  controllers: [QuestionnaireController],
   exports: [],
 })
 export class ExtractionModule {}
